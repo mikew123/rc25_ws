bbcc9dcfe1dc49a5b86264154fccc53536504d58
mikew <mikew123@gmail.com>
2025-09-18 16:31:37 -0500

    cone node : filtered cone detect based on bounding box size

--- src/robocolumbus25_stuff/robocolumbus25_stuff/robocolumbus25_cone_node.py
+++ src/robocolumbus25_stuff/robocolumbus25_stuff/robocolumbus25_cone_node.py
@@ -1,4 +1,5 @@
 import rclpy
+import math
 
 from rclpy.node import Node
 from geometry_msgs.msg import PointStamped
@@ -46,32 +47,45 @@ class ConeNode(Node):
             num_detections +=1
             #header = detection.header
             results = detection.results
+            bbox = detection.bbox
+            bboxSize = bbox.size
             for result in results :
                 pose = result.pose.pose
                 position = pose.position
                 x = position.x
                 y = position.y
                 z = position.z
-                # self.get_logger().info(f"cone_det_subscription_callback: {stamp=} {x=:.3f} {y=:.3f} {z=:.3f}")
+                if z==0 : continue
 
-                # Use median-5 filter to remove points with distance spikes
-                m5 = self.m5_filter
-                # add new sample to filter list memory
-                m5 = [(x,y,z),m5[0],m5[1],m5[2],m5[3]]
-                self.m5_filter = m5
-                # sort tupple[2] z is distance (m5 is not modified)
-                m5s = sorted(m5, key=lambda xyz: xyz[2])
-                # pick the median sample
-                (xm,ym,zm) = m5s[2]
-                #(xm,ym,zm) = m5[2] # DEBUG: unfiltered
-                #(xm,ym,zm) = (x,y,z)
-                #self.get_logger().info(f"{m5=} {m5s=}")
-                
-                # Publish the cone location point x,y,z relative to camera
-                pmsg.point.x = zm # Forward meters
-                pmsg.point.y = -xm # Side meters (-x fixes rviz location mapping)
-                pmsg.point.z = ym # Elevation center of cone (on level ground)
-                self.cone_point_publisher.publish(pmsg)
+                # use bounding box to "filter" non-cones
+                bx0 = bboxSize.x
+                by0 = bboxSize.y
+                bx1 = 33*(2.8/z) # z is distance
+                by1 = 59*(2.8/z)
+                xx = math.fabs(1.0 - bx0/bx1)
+                yy = math.fabs(1.0 - by0/by1)
+                if z>1 : pct = 0.2
+                else : pct = 0.5
+                if (xx < pct) and (yy < pct) :
+                    # Use median-5 filter to remove points with distance spikes
+                    m5 = self.m5_filter
+                    # add new sample to filter list memory
+                    m5 = [(x,y,z),m5[0],m5[1],m5[2],m5[3]]
+                    self.m5_filter = m5
+                    # sort tupple[2] z is distance (m5 is not modified)
+                    m5s = sorted(m5, key=lambda xyz: xyz[2])
+                    # pick the median sample
+                    (xm,ym,zm) = m5s[2]
+                    #(xm,ym,zm) = m5[2] # DEBUG: unfiltered
+                    #(xm,ym,zm) = (x,y,z)
+                    #self.get_logger().info(f"{m5=} {m5s=}")
+                    
+                    # Publish the cone location point x,y,z relative to camera
+                    pmsg.point.x = zm # Forward meters
+                    pmsg.point.y = -xm # Side meters (-x fixes rviz location mapping)
+                    pmsg.point.z = ym # Elevation center of cone (on level ground)
+                    self.cone_point_publisher.publish(pmsg)
+                    # self.get_logger().info(f"cone_callback: {stamp=} x={zm:.3f} y={-xm:.3f} z={ym:.3f} {bboxSize=}")
 
         if num_detections == 0 :
             # publish invalid cone location at 0,0 
